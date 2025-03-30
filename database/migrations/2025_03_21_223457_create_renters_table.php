31712e9a262e0f05d81effd86641caddf7cb9552
phyominhein <phyominhein321@gmail.com>
2025-03-30 17:27:43 +0900

    secondCommit

--- database/migrations/2025_03_21_223457_create_renters_table.php
+++ database/migrations/2025_03_21_223457_create_renters_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('renters', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->timestamps();
+            $table->auditColumns();
         });
     }
 
